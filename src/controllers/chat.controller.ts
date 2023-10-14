649398b36a0444ef53743cf2a929466b19d1d839
Levy <levymcruz@gmail.com>
2023-10-14 19:10:53 -0300

    att

--- src/controllers/chat.controller.ts
+++ src/controllers/chat.controller.ts
@@ -3,7 +3,6 @@ import httpStatus from "http-status";
 import { chatService } from "../services/chat.services";
 
 async function postMessage(req: Request, res: Response) {
-  console.log(req.body)
   const response = await chatService.postMessage(req.body);
   res.status(httpStatus.OK).json(response);}
 
--- src/services/chat.services.ts
+++ src/services/chat.services.ts
@@ -4,20 +4,16 @@ import { Configuration, OpenAIApi } from "openai";
 import { questions, allResponses } from "../database/db";
 import { Questions } from "protocols/protocols";
 
-async function postMessage({ questions, previousPrompt, isFirstChat }) {
-  const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
-  const openai = new OpenAIApi(configuration);
+async function postMessage({ questions, isFirstChat, previousPrompt  }) {
   let text = ""
   let prompt = Boolean(isFirstChat) ? concatenate(text, questions) : concatenatePrevious(previousPrompt, questions)
-  const completion = await openai.createCompletion({
-    model: "gpt-3.5-turbo-instruct",
-    prompt,
-    temperature: 0.6,
-  });
-
-  const returnedQuestion = completion.data.choices[0].text.replace("\n", '');
+  console.log(prompt)
+  let returnedQuestion = ""
+  while (returnedQuestion.length === 0) {
+    let completion = await handleGPT(prompt)
+    // console.log(completion.data.choices[0])
+    returnedQuestion = completion.data.choices[0].text.replace("\n", '');
+  }
 
   return {
     createdQuestion: returnedQuestion.split('?')[0].replace(/[^a-zA-ZÀ-ú\s]/g, ""),
@@ -25,10 +21,22 @@ async function postMessage({ questions, previousPrompt, isFirstChat }) {
   }
 }
 
+async function handleGPT (prompt: string) {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+  const completion = await openai.createCompletion({
+    model: "gpt-3.5-turbo-instruct",
+    prompt,
+    temperature: 0.6,
+  });
+  return completion
+}
 
 function concatenatePrevious(previousPrompt: string, questions: Questions[]){
   questions.forEach(element => {
-    previousPrompt += element.question + ": " + element.answer + " \n "
+    previousPrompt += " | " + element.question + ": " + element.answer + " \n "
     allResponses.push({question: element.question, awns: element.answer})
   });
   return previousPrompt
@@ -36,7 +44,7 @@ function concatenatePrevious(previousPrompt: string, questions: Questions[]){
 
 function concatenate(text: string, questions: Questions[]){
   questions.forEach(element => {
-    text += element.question + ": " + element.answer + " \n "
+    text += " | " + element.question + ": " + element.answer + " \n "
     allResponses.push({question: element.question, awns: element.answer})
   });
   const prompt = `Sempre faça apenas uma pergunta e RETORNE APENAS A STRING, sem texto explicativo. Retorne essa pergunta (apenas a pergunta) em formato STRING "SUA PERGUNTA". Nunca repita uma pergunta já feita. NÃO RESPONDA A PERGUNTA FEITA. O intuito dessa pergunta é construir uma descrição melhor detalhada do anuncio do produto. Segue as perguntas e respostas: `
