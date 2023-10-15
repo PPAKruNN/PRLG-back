35de470d9f7e8251432f141cbdb5176f2e31ed4f
Levy <levymcruz@gmail.com>
2023-10-14 23:43:26 -0300

    moving first 3 questions to postMessage

--- src/controllers/chat.controller.ts
+++ src/controllers/chat.controller.ts
@@ -3,7 +3,10 @@ import httpStatus from "http-status";
 import { chatService } from "../services/chat.services";
 import { allResponses } from "../database/db";
 
+let counter = 0;
+
 async function postMessage(req: Request, res: Response) {
+  req.body.counter = counter;
   const response = await chatService.postMessage(req.body);
 
   res.status(httpStatus.OK).json(response);
